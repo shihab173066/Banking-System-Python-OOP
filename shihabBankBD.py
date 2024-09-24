5839ef702f81b46ac1face294274cf4ef30457ed
shihab173066 <shihab.hossainSH@gmail.com>
2024-09-24 23:09:37 +0600

    The first version of the code

--- shihabBankBD.py
+++ shihabBankBD.py
@@ -6,4 +6,24 @@ methods Account Creation, Deposit Functionality, Withdrawal Functionality, Balan
 
 """
 
-print("Hello Bank!!")
+class shihabBankBD:
+
+    def __init__(self, name, initial_diposit, unique_account_num):
+        self.name = name
+        self.unique_account_num = int(unique_account_num)
+        self.initial_diposit = float(initial_diposit)
+
+    def account_create(self):
+        if len(str(self.unique_account_num)) == 6 and self.initial_diposit >= 3000.00:
+            return (f"Account is created for {self.name}, with an account number of {self.unique_account_num}, "
+      f"and a mandatory deposit of {self.initial_diposit:.2f}")
+        else:
+            return ("Please follow instructions properly!!")
+        
+    
+
+
+obj = shihabBankBD("Shihab", 3000.00, 123456)
+print(obj.account_create())
+
+
