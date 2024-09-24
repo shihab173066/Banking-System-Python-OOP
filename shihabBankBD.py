964815c64194ba62dd02620185636940fb8bc445
shihab173066 <shihab.hossainSH@gmail.com>
2024-09-24 22:16:48 +0600

    Initial stage of program

---
+++ shihabBankBD.py
@@ -0,0 +1,9 @@
+"""
+Make Class shihabBankBD
+
+constructor takes name(str), unique_account_num(int only) and initial_diposit(float)
+methods Account Creation, Deposit Functionality, Withdrawal Functionality, Balance Inquiry
+
+"""
+
+print("Hello Bank!!")
